a4b1dfdf39c4d6e89ff11c088bb3c77a2e917dbe
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-26 10:47:13 -0300

    dev-com-site_prism-envs

--- Aulas - SitePrism/features/support/env.rb
+++ Aulas - SitePrism/features/support/env.rb
@@ -5,5 +5,8 @@ require 'site_prism'
 require 'rspec'
 require 'capybara/cucumber'
 
+ENVIRONMENT = ENV['ENVIRONMENT']
+puts ENVIRONMENT
+
 Capybara.default_driver = :selenium_chrome
 Capybara.app_host = 'https://www.google.com.br'
