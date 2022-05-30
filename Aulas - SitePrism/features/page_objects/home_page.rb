70189bb6002312ad18940dd34220111b282e4b3b
Rafael Schettini <rafaschettini902@gmail.com>
2022-05-30 14:35:07 -0300

    back_to_google_map

--- Aulas - SitePrism/features/page_objects/home_page.rb
+++ Aulas - SitePrism/features/page_objects/home_page.rb
@@ -1,26 +1,7 @@
-class FooterNewsletter < SitePrism::Section
-    
-  element :send_button, 'button[class="btn btn-block btn-primary"]'
-
-end
-
-class ProductsHeadset < SitePrism::Section
-
-  element :price, '.new-price'
-  element :name, 'h3 a'
-
-end
-
 class Home < SitePrism::Page
     
   set_url('/')
 
-  element :btn_open_search, 'div[class="search col-sm-3 col-xs-3 text-center"] .menu-icon'
-    
-  elements :menu_itens, 'ul[class="menu hidden-sm hidden-xs list-inline"] li div[class="submenu"]'
-
-  section :footer_newsletter, FooterNewsletter, 'footer div[class="newsletter"]'
-
-  sections :products_headset, ProductsHeadset, 'div[class="row"] div[class="col-md-3 col-sm-4 col-xs-6 text-center"]'
+  element :logo, 'img[class="lnXdpd"]'
 
 end
